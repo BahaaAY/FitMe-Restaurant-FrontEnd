50f46ff7989987fafc152caa31d887a5bb2883cd
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-04-29 21:58:51 +0300

    finished pages

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,29 @@
 let searchBar = document.getElementById("search-bar");
 searchBar.setAttribute("size", searchBar.getAttribute("placeholder").length);
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 13,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+  },
+});
